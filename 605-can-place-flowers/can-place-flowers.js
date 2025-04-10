5f8e9bce14c26c5a248fff42c1c021cfac58d04d
Noman Ahmad <nomanahmad.dev@gmail.com>
2025-04-10 16:05:23 -0400

    Time: 1 ms (85.38%) | Memory: 57.7 MB (21.18%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.js
+++ 605-can-place-flowers/can-place-flowers.js
@@ -6,11 +6,9 @@
 var canPlaceFlowers = function(flowerbed, n) {
     let newFlowersPlanted = 0; 
     for (let i = 0; i < flowerbed.length; i++) {
-        const alreadyPlanted = flowerbed[i] === 1; 
-        const leftPlanted = (i === 0 ? false : flowerbed[i-1] === 1 ); 
-        const rightPlanted = (i === flowerbed.length - 1 ? false : flowerbed[i+1] === 1); 
+        const cantPlant = (flowerbed[i] === 1) || ((i === 0 ? false : flowerbed[i-1] === 1 )) || ((i === flowerbed.length - 1 ? false : flowerbed[i+1] === 1)); 
 
-        if(!(alreadyPlanted || rightPlanted || leftPlanted)) {
+        if(!cantPlant) {
             flowerbed[i] = 1; 
             newFlowersPlanted++;
         }
