f27de681c04504ee8970794f1658419d045c9879
Noman Ahmad <nomanahmad.dev@gmail.com>
2025-04-15 18:08:00 -0400

    Time: 37 ms (65.32%) | Memory: 17.8 MB (48.91%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,21 +1,21 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         merged_string = ''
-
-        if len(word1) < len(word2):
-            for i in range(0, len(word1), 1):
-                merged_string = merged_string + word1[i] + word2[i]
-            
-            merged_string = merged_string + word2[-(len(word2)-len(word1)):]
-
-        elif len(word2) < len(word1):
-            for i in range(0, len(word2), 1):
-                merged_string = merged_string + word1[i] + word2[i]
-            
-            merged_string = merged_string + word1[-(len(word1)-len(word2)):]
+        concat_str = '' 
+        max_itr = 0
         
-        else: 
-            for i in range(0, len(word2), 1):
-                merged_string = merged_string + word1[i] + word2[i]
+        if len(word2) > len(word1):
+            concat_str = word2[-(len(word2) - len(word1)):]
+            max_itr = len(word1)
+        elif len(word1) > len(word2):
+            concat_str = word1[-(len(word1) - len(word2)):]
+            max_itr = len(word2)
+        else:
+            max_itr = len(word1)
+
+        for i in range(0, max_itr, 1):
+            merged_string += word1[i] + word2[i]
+
+        merged_string += concat_str
 
         return merged_string
